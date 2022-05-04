3e771c6605b8fd8cd36d037f8e829bbbb346acf9
Zim Agu <zimmagu@gmail.com>
2022-05-03 18:49:05 -0700

    allauth set up

--- storeFront/settings.py
+++ storeFront/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'coreFunctionality'
+    'django.contrib.sites',
+
+    'coreFunctionality',   
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +72,15 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+# Authentication 
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
 WSGI_APPLICATION = 'storeFront.wsgi.application'
@@ -122,3 +137,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+SITE_ID = 1
