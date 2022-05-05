0bae6ad4d8bd96cc4b35fb904d581e0887a3c271
Zim Agu <zimmagu@gmail.com>
2022-05-05 09:41:48 -0700

    Models registered to the admin site

--- coreFunctionality/admin.py
+++ coreFunctionality/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import item, orderItem, order
+
+admin.site.site_header = "Administration for Zim's Ecommerce Site"  # Changes the header of the adming page
+
+admin.site.register(item)       # Registers the item model on to the database
+admin.site.register(orderItem)  # Registers the orderItem model on to the database
+admin.site.register(order)      # Registers the order model on to the database
